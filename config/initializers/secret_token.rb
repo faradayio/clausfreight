9d1993eda85dba98daa03d5cdffb7f91f09e1568
Andy Rossmeissl <andy@rossmeissl.net>
2010-12-13 09:07:50 -0600

    rails new clausfreight

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clausfreight::Application.config.secret_token = '882d0924525b07b88f5dbafb9aa4cafd018e78f2a646f880068c8c26f6af349db058627c365e51092f016d937f73cc2d3cb5433ee7c8dc9a6613d3a9e5684bf8'

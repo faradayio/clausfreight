6dcb94b382b0a6b9705834c9c08cdc1be09f43b4
Andy Rossmeissl <andy@rossmeissl.net>
2010-12-13 22:36:53 -0600

    Start over link

--- app/controllers/presents_controller.rb
+++ app/controllers/presents_controller.rb
@@ -1,6 +1,6 @@
 class PresentsController < ApplicationController
   def new
-    @present = Present.new
+    @present = Present.new((params[:present] || {}).slice(:destination))
   end
   def create
     @present = Present.new params[:present]
